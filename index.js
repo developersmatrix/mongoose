a72bdd270c8632ece6af5a65cc6fea11231cb0f9
neo <developersmatrix3x3@gmail.com>
2022-01-04 16:09:45 +0530

    User model added

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("hello" + 2);
+const mongoose = require("mongoose");
+
+const User = require('./Models/user');
+
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("connected"))
+    .catch((error) => console.log(error));
+
+
+const run = async () => {
+
+    try {
+        const user = new User({
+            name: 'Bodi',
+            age: 30
+        })
+
+        const response = await user.save()
+        console.log(response);
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+
+}
+
+// run()
+
+// const user = new User({
+//     name: 'eshaan',
+//     age: 4
+// })
+
+// user.save().then(res => console.log(res)).catch(err => console.log(err))
+
+User.find().exec().then(res => console.log(res)).catch(err => console.log(err))
