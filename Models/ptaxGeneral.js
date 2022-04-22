7731738692afc54c77bbc11b13d2955771bba0b9
neo <developersmatrix3x3@gmail.com>
2022-04-22 22:50:24 +0530

    for scratch

--- Models/ptaxGeneral.js
+++ Models/ptaxGeneral.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ptaxGeneralSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    startYear: String,
-    endYear: String,
-    totalNoOfProperties: String,
-    openingBalance: String,
-    currentYearDemand: String
-})
+  _id: mongoose.Schema.Types.ObjectId,
+  startYear: String,
+  endYear: String,
+  totalNoOfProperties: String,
+  openingBalance: String,
+  currentYearDemand: String,
+});
 
 module.exports = mongoose.model("PtaxGeneral", ptaxGeneralSchema);
--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const mongoose = require("mongoose");
 
-// const User = require('./Models/user');
-// const PtaxCollection = require('./Models/ptaxCollection');
-// const PtaxGeneral = require("./Models/ptaxGeneral");
-// const Ptax = require("./Models/ptax");
+const PtaxGeneral = require("./Models/ptaxGeneral");
+const ptaxColl = require("./Models/monthlyCollection");
+
+const data = new PtaxGeneral({
+  _id: new mongoose.Types.ObjectId(),
+  startYear: "2021",
+  endYear: "2022",
+  totalNoOfProperties: "4546",
+  openingBalance: "5.08",
+  currentYearDemand: "75.00",
+});
+
+// const data = ptaxColl({
+//   _id: new mongoose.Types.ObjectId(),
+//   year: "2022",
+//   month: "March",
+//   OBC: "2.03",
+//   CYD: "4.26",
+// });
+
+// data
+//   .save()
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+const startYear = 2022;
+const endyear = 2023;
+const currentMonth = "May";
+
+PtaxGeneral.find().where({ startYear: startYear });
 
 mongoose
   .connect(process.env.MONGODB_URI)
